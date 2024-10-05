1b6f6dda9dbb499af38b66f1c221f9704f319904
WANGXING130 <3551764468@qq.com>
2024-10-05 20:13:44 +0800

    bul-on-off.js

--- Oct-vacation/Task4/resources/js/bul-on-off.js
+++ Oct-vacation/Task4/resources/js/bul-on-off.js
@@ -3,11 +3,11 @@ function changeImage()
    element=document.getElementById('myimage')
     if (element.src.match("bulbon"))
     {
-        element.src="/images/pic_bulboff.gif";
+        element.src="../resources/imgs/pic_bulboff.gif";
     }
     else
     {
-        element.src="/images/pic_bulbon.gif";
+        element.src="../resources/imgs/pic_bulbon.gif";
     }
   // TODO
   // 在此处，实现小灯开关的功能！
